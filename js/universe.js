3aa602e4c85969ec1e2f97ea8f9652170eeff5ad
skojha32 <skojha32@gmail.com>
2019-04-26 23:40:05 +0530

    Final Push

---
+++ js/universe.js
@@ -0,0 +1,67 @@
+
+    /* stats-counter */
+        $('.counter').countUp();
+    /* /stats-counter */
+
+    /* owl carousel */
+        $(document).ready(function () {
+            var owl = $('.owl-carousel');
+            owl.owlCarousel({
+                margin: 10,
+                nav: true,
+                loop: true,
+                responsive: {
+                    0: {
+                        items: 1
+                    }
+                }
+            })
+        })
+    /* //owl carousel */
+
+    /* start-smooth-scrolling */
+        jQuery(document).ready(function ($) {
+            $(".scroll").click(function (event) {
+                event.preventDefault();
+
+                $('html,body').animate({
+                    scrollTop: $(this.hash).offset().top
+                }, 1000);
+            });
+        });
+    /* //end-smooth-scrolling */
+
+    /* dropdown nav */
+        $(document).ready(function () {
+            $(".dropdown").hover(
+                function () {
+                    $('.dropdown-menu', this).stop(true, true).slideDown("fast");
+                    $(this).toggleClass('open');
+                },
+                function () {
+                    $('.dropdown-menu', this).stop(true, true).slideUp("fast");
+                    $(this).toggleClass('open');
+                }
+            );
+        });
+    /* //dropdown nav */
+
+    /* smooth-scrolling-of-move-up */
+	
+        $(document).ready(function () {
+            /*
+            var defaults = {
+                containerID: 'toTop', // fading element id
+                containerHoverID: 'toTopHover', // fading element hover id
+                scrollSpeed: 1200,
+                easingType: 'linear' 
+            };
+            */
+
+            $().UItoTop({
+                easingType: 'easeOutQuart'
+            });
+
+        });
+
+    /* //smooth-scrolling-of-move-up */
